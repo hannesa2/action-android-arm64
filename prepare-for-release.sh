48b1eeb2b9367a74aea5b22b4c594585577d4051
Hannes Achleitner <hannes.software@gmx.at>
2026-03-28 10:58:22 +0100

    Improve release script
    Cosmetic in prepare-for-release.sh
    
    Add branch for each release

--- prepare-for-release.sh
+++ prepare-for-release.sh
@@ -3,5 +3,13 @@
 set -ex
 
 for i in emulator-run-cmd install-sdk; do
-  (cd $i && docker run -t -v $(pwd):/opt/app -w /opt/app node:24 bash -c 'npm install && npm run build && npm prune --production' && git add -f node_modules lib)
+  echo "=== Processing $i ==="
+  cd $i
+  docker run -t -v $(pwd):/opt/app -w /opt/app node:24 bash -c 'npm install && npm audit fix && npm run build && npm prune --production'
+  git add -f node_modules
+  git add -f lib || echo "$1 lib directory not found, skipping"
+  cd ..
 done
+
+git checkout -b release-$(git rev-list HEAD --count)
+git commit -m "Add output of ./prepare-for-release.sh"
